b6adacfef817ec8d8f943b428db4572aba1379ec
Olexandra <olexandra.dmytrenko@gmail.com>
2016-03-21 13:10:53 +0200

    Updated tests so that they really check. Second test - tried list and when statement with Dates.

--- src/test/java/myhibernate/PromoCardTest.java
+++ src/test/java/myhibernate/PromoCardTest.java
@@ -5,11 +5,15 @@ import hibernate.HibernateUtil;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.criterion.Projections;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -18,6 +22,13 @@ import static org.junit.Assert.assertEquals;
  */
 public class PromoCardTest {
     private static Logger log = LoggerFactory.getLogger(PromoCardTest.class);
+    private static Session session = null;
+
+    @BeforeClass
+    public static void setUp(){
+        //Get Session
+        session = HibernateUtil.getSessionAnnotationFactory().getCurrentSession();
+    }
 
     @Test
     public void addOnePromoCard() {
@@ -26,6 +37,7 @@ public class PromoCardTest {
         //promoCard.setInsertTime(new Date());
 
         //Get Session
+
         Session session = HibernateUtil.getSessionAnnotationFactory().getCurrentSession();
 
         //start transaction
@@ -33,7 +45,7 @@ public class PromoCardTest {
 
         //last values before addition of row
         Number numbOfLinesBeforeAdd = (Number) session.createCriteria("domain.PromoCard").setProjection(Projections.rowCount()).uniqueResult();
-//        int idBeforeSave = (int) session.createCriteria("domain.PromoCard").setProjection(Projections.max("id")).uniqueResult();
+        int idBeforeSave = ((Long) session.createCriteria("domain.PromoCard").setProjection(Projections.max("id")).uniqueResult()).intValue();
 
         //Save the Model object
         session.save(promoCard);
@@ -41,7 +53,7 @@ public class PromoCardTest {
         //last values after addition of row
         long idAfterSave = promoCard.getId();
         Number numbOfLinesAfterAdd = (Number) session.createCriteria("domain.PromoCard").setProjection(Projections.rowCount()).uniqueResult();
-//        Address addedAddress = (Address) session.get(Address.class, idAfterSave);
+        PromoCard addedPromoCard = (PromoCard) session.get(PromoCard.class, idAfterSave);
 
         //Commit transaction
         session.getTransaction().commit();
@@ -49,19 +61,20 @@ public class PromoCardTest {
         //Assertions
         int amountOfAddedRows = numbOfLinesAfterAdd.intValue() - numbOfLinesBeforeAdd.intValue();
         log.info("Difference between amount of rows before addition {} and after addition {} is: {}", numbOfLinesBeforeAdd, numbOfLinesAfterAdd, amountOfAddedRows);
-//        log.info("ID before save is {}, ID after save is {}", idBeforeSave, idAfterSave);
-//        log.info("The promoCard read from the Address: {} with id {}", addedAddress.toString(), addedAddress.getId());
+        log.info("ID before save is {}, ID after save is {}", idBeforeSave, idAfterSave);
+        log.info("The PromoCard is read after Save: {} with id {}", addedPromoCard.toString(), addedPromoCard.getId());
 
         //terminate session factory, otherwise program won't end
         HibernateUtil.getSessionAnnotationFactory().close();
         assertEquals("There were more or less than one added rows", 1, amountOfAddedRows);
-//        assertEquals("ID is not greater than one than the last previous id was", ++idBeforeSave, idAfterSave);
-//        assertEquals("The added promoCard is wrong", promoCard.toString(), addedAddress.toString());
+        assertEquals("ID is not greater than one than the last previous id was", ++idBeforeSave, idAfterSave);
+        assertEquals("The added promoCard is wrong", promoCard.toString(), addedPromoCard.toString());
 
     }
 
     @Test
-    public void addSetOfAddresses() {
+    public void addAndSearchPromoCardByDate() {
+        //Given
         PromoCard promoCard = new PromoCard(200.02);
         promoCard.setBlockedAmount(100.43);
         LocalDateTime myDate = LocalDateTime.of(2015, 04, 30, 15, 43, 12);
@@ -70,24 +83,24 @@ public class PromoCardTest {
 
         //Get Session
         Session session = HibernateUtil.getSessionAnnotationFactory().getCurrentSession();
-        int numberOfRecordsBeforeAddition = 0, numberOfRecordsAfterAddition = 0;
         int amountOfAddedLines = 1;
+        List<Long> idOfDateBeforeAdd = new ArrayList<>(), idOfDateAfterAdd = new ArrayList<>();
 
         try {
             //start transaction
             session.beginTransaction();
-            numberOfRecordsBeforeAddition = ((Long) session.createQuery("SELECT COUNT(*) FROM domain.PromoCard").uniqueResult()).intValue();
-            session.saveOrUpdate(promoCard);
             log.info("Want to write PromoCard {}", promoCard);
-            log.info("Want to write PromoCard {}", promoCard);
+            idOfDateBeforeAdd = getRecordsWithDate(myDate, session);
 
-            numberOfRecordsAfterAddition = ((Long) session.createQuery("SELECT COUNT(*) FROM domain.PromoCard").uniqueResult()).intValue();
-//            int numberOfIdOfDate = ((Long) session.createQuery("SELECT count(*) FROM domain.PromoCard WHERE CREATE_DATE='2015-04-30 15:43:12'").uniqueResult()).intValue();
-//            int numberOfIdOfDate = ((Long) session.createQuery("SELECT count(*) FROM domain.PromoCard WHERE CREATE_DATE= :my_date")
-//                    .setParameter("my_date", "2015-04-30 15:43:12").uniqueResult()).intValue();
-            int numberOfIdOfDate = ((Long) session.createQuery("SELECT count(*) FROM domain.PromoCard WHERE CREATE_DATE= :my_date")
-                    .setParameter("my_date", myDate.toString()).uniqueResult()).intValue();
-            log.info("Count of records with date '{}' is {}", myDate, numberOfIdOfDate);
+            //When
+            session.saveOrUpdate(promoCard);
+
+            //Then
+            idOfDateAfterAdd = getRecordsWithDate(myDate, session);
+            log.info("Record ids with date '{}' \n" +
+                            "before update: amount = {}, ids = {} \n" +
+                            "after  update: amount = {}, ids = {}",
+                    myDate, idOfDateBeforeAdd.size(), idOfDateBeforeAdd, idOfDateAfterAdd.size(), idOfDateAfterAdd);
             session.getTransaction().commit();
 
         } catch (HibernateException e) {
@@ -99,7 +112,13 @@ public class PromoCardTest {
         HibernateUtil.getSessionAnnotationFactory().close();
 
         assertEquals(String.format("There were not %d lines added", amountOfAddedLines),
-                numberOfRecordsBeforeAddition + amountOfAddedLines, numberOfRecordsAfterAddition);
+                idOfDateBeforeAdd.size() + amountOfAddedLines, idOfDateAfterAdd.size());
+    }
+
+    private List getRecordsWithDate(LocalDateTime myDate, Session session) {
+        return session.createQuery("SELECT id FROM domain.PromoCard WHERE CREATE_DATE= :my_date")
+                .setParameter("my_date", myDate.toString())
+                .list();
     }
 
 }
